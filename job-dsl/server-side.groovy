94ae39e7a20b53ae035231fdbf584fdf4ce83c5b
Ishaan Agarwal <IshaanAgarwal2@Ishaans-MacBook-Pro.local>
2024-08-06 14:33:08 +0530

    trying Job DSL

---
+++ job-dsl/server-side.groovy
@@ -0,0 +1,25 @@
+job("Jenkins-test") {
+    scm {
+        git("git://github.com/Ishaan1604/Jenkins-test.git") { node -> 
+            node / gitConfigName("DSL User")
+            node /gitConfigEmail("jenkins-dsl@newtech.academy")
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName("ishaan04/jenkins-test")
+            tag('$(GIT_REVISION, length=9)')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
